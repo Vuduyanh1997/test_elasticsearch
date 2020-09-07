76a70698c3832c3f42297cffb6fb2a48853bab14
Duy Anh <vuduyanh.bn.97@gmail.com>
2020-09-07 13:38:34 +0700

    source code

---
+++ resources/views/search.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Tìm kiếm</title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous"/>
+	<style type="text/css" media="screen">
+		
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<center><h1>Tìm kiếm</h1></center>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 form-group">
+				<label for="search_name">Nhập tên</label>
+				<input type="text" id="search_name" name="search_name" class="form-control">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
